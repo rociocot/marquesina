911b602a0e8b1751ae2d0c4e74a3530a563d94dc
rociocot <rocioacot@gmail.com>
2026-09-03 20:29:17 -0300

    Entidades Producto y ConfiguracionTv

---
+++ src/main/java/com/cartelera/backend/model/ConfiguracionTv.java
@@ -0,0 +1,37 @@
+package com.cartelera.backend.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table (name = "configuraciones_tv")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ConfiguracionTv {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private Integer nroTv;
+
+    private String tipoLetra;
+
+    private String tamanoLetra;
+
+    private String posicionTexto;
+
+    private String posicionImagen;
+
+    private String tituloCabecera;
+
+    private String imagenUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String estiloCssCustom;
+
+}
---
+++ src/main/java/com/cartelera/backend/model/Producto.java
@@ -0,0 +1,35 @@
+package com.cartelera.backend.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table (name = "productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String nombre;
+
+    private String descripcion;
+
+    private BigDecimal precio;
+
+    @Column(nullable=false)
+    private String imagenUrl;
+
+    @Column(nullable = false)
+    private Boolean disponible;
+
+    private Integer asignadoTv;
+}
+
